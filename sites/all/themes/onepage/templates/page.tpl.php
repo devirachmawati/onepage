019613d46b8d98400d23868969758e24cb97dbc3
Devi Rachmawati <devi.rachmawati@mavenlab.com>
2015-03-13 15:47:02 +0700

    Update style and page.tpl code

--- sites/all/themes/onepage/templates/page.tpl.php
+++ sites/all/themes/onepage/templates/page.tpl.php
@@ -168,6 +168,30 @@
         });
 
       });
+
+    /* Magnific Pop Up image on Gallery*/
+    $(document).ready(function() {
+      $('.mfp-gallery-image').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+          enabled: true,
+          navigateByImgClick: true,
+          preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
+        image: {
+          tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+          titleSrc: function(item) {
+            // return item.el.attr('class') + '<small>by YourSelf</small>';
+            return item.el.find('img').attr('title') + '<small>by YourSelf</small>';
+          }
+        }
+      });
+    });
+
     })(jQuery);
+
   </script>
     <script type="text/javascript">
